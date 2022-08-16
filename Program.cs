ee9041079e6d7b2f4fd509ede2d42d4f1242d5f5
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-08-16 15:19:09 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -20,3 +20,24 @@ else
 {
     Console.WriteLine($"введенное число {number} является палиндромом!");
 }
+
+// Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координату X1: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y1: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z1: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату X2: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y2: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z2: ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+double distanceAB;
+Console.WriteLine(distanceAB = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2)));
+
+
